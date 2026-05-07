5327fb66184ebed82d68a529b69a46c64b81e578
Dougal <dougalwentworth@gmail.com>
2026-05-07 16:02:11 +0100

    First commit

--- Training/python/train_BDT.py
+++ Training/python/train_BDT.py
@@ -11,6 +11,7 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser(description="XGBoost Classifier Training")
     parser.add_argument('--channel', type=str, help="Channel to train", required=True)
+    parser.add_argument('--config', type=str, help="Path to config yaml file", required=False, default='../config/tt/BDTconfig.yaml')
     # parser.add_argument('--cut', type=str, help="VSjet cut to be used", required=False)
     parser.add_argument('--gpu', action='store_true', help="Use GPU for training")
     return parser.parse_args()
@@ -120,7 +121,7 @@ def main():
     args = get_args()
     if args.channel == 'tt': # Use VTight VSjet
         print("Training for tt channel (VTight Vsjet cut)")
-        cfg = yaml.safe_load(open("../config/tt/BDTconfig.yaml"))
+        cfg = yaml.safe_load(open(args.config))
         cfg['Setup']['model_prefix'] = 'model' # begining of model json name (add parity after)
     elif args.channel == 'mt':
         print("Training for MuTau channel")
